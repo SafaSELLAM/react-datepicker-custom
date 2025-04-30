f7a1d923bdc1ea5b1f9d22d634fcae925673d465
SafaSELLAM <safa.sellam@outlook.fr>
2025-04-30 22:09:29 +0200

    Add datepicker component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+import { DatePicker } from './Components/DatePicker.jsx'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <DatePicker lang="fr" />
+    </div>
+  )
+}
+
+export default App
