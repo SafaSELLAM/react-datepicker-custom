a9176de441574d6acd0e6d3b554691d5dc674dff
SafaSELLAM <safa.sellam@outlook.fr>
2025-05-29 11:09:12 +0200

    add comments and components to the main component

--- src/Components/DatePicker.jsx
+++ src/Components/DatePicker.jsx
@@ -1,10 +1,11 @@
-import { faHome } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
-import { formatDate, formatInputDate, getDaysInMonth, getFirstDayOfMonth, isDateInRange, isSameDay } from "../dateFunctions"
+import { formatDate, formatInputDate, getDaysInMonth, getFirstDayOfMonth, parseDateFromString } from "../dateFunctions"
+import { CalendarGrid } from "./CalendarGrid.jsx"
+import { CalendarHeader } from "./CalendarHeader.jsx"
+import { Weekdays } from "./Atoms/Weekdays.jsx"
 
-export const DatePicker = ({ lang = "en", minDate, maxDate }) => {
+export const DatePicker = ({ lang, minDate, maxDate }) => {
     const { i18n, t } = useTranslation()
 
     const [selectedDate, setSelectedDate] = useState("")
@@ -19,58 +20,30 @@ export const DatePicker = ({ lang = "en", minDate, maxDate }) => {
     const toggleCalendar = () => {
         setIsOpen(!isOpen)
     }
+
+    // Function to handle input change and format the date
     useEffect(() => {
         const defaultDate = new Date()
+
         if (selectedDate) {
-            const parts = selectedDate.split("/")
-
-            let day, month, year
-
-            if (lang === "fr" && parts.length === 3) {
-                [day, month, year] = parts
-            } else if (lang === "en" && parts.length === 3) {
-                [month, day, year] = parts
-            }
-
-            if (day && month && year) {
-                const dateObj = new Date(year, Number(month) - 1, Number(day))
-                if (!isNaN(dateObj)) {
-                    setCurrentDate(dateObj)
-                }
+            const parsed = parseDateFromString(selectedDate, lang)
+            if (parsed) {
+                setCurrentDate(parsed)
             }
         } else {
             setCurrentDate(defaultDate)
         }
     }, [selectedDate, lang])
 
-    //formate Date
-    // const formatDate = (date) => {
-    //     return new Intl.DateTimeFormat(lang === "fr" ? "fr-FR" : "en-US").format(date)
-    // }
-
+    // Function to handle date selection
     const handleDateClick = (day) => {
         const newDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), day)
-        setSelectedDate(formatDate(newDate))
+        setSelectedDate(formatDate(newDate, lang))
         setIsOpen(false)
     }
 
-    // const formatInputDate = (value) => {
-
-    //     const numbers = value.replace(/\D/g, "")
-
-    //     if (numbers.length <= 2) return numbers
-    //     if (numbers.length <= 4) return `${numbers.slice(0, 2)}/${numbers.slice(2)}`
-    //     return `${numbers.slice(0, 2)}/${numbers.slice(2, 4)}/${numbers.slice(4, 8)}`
-
-    // }
-
-
-    // create date with the first day of the next month, day = 0 => last day of the current month 
-    // const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate()
-
-    //return the day of the first day of the current month 
-    // const getFirstDayOfMonth = (year, month) => new Date(year, month, 1).getDay()
-
+    // Function to go to the previous month
+    // Check if the previous month is within the minDate range
     const goToPreviousMonth = () => {
         const prev = new Date(currentDate)
         prev.setMonth(prev.getMonth() - 1)
@@ -79,6 +52,8 @@ export const DatePicker = ({ lang = "en", minDate, maxDate }) => {
         }
     }
 
+    // Function to go to the next month
+    // Check if the next month is within the maxDate range
     const goToNextMonth = () => {
         const next = new Date(currentDate)
         next.setMonth(next.getMonth() + 1)
@@ -87,14 +62,12 @@ export const DatePicker = ({ lang = "en", minDate, maxDate }) => {
         }
     }
 
+    // Get the number of days in the current month and the first day of the month
     const daysInMonth = getDaysInMonth(currentDate.getFullYear(), currentDate.getMonth())
     const firstDayOfMonth = getFirstDayOfMonth(currentDate.getFullYear(), currentDate.getMonth())
 
-    //const daysArray = [];
-    // add empty spots
-    /*for (let i = 0; i < firstDayOfMonth; i++) {
-        daysArray.push(null);
-    }*/
+
+    // Create an array of null values for the days before the first day of the month
     const daysArray = Array(firstDayOfMonth).fill(null)
 
     //push the days in the month 
@@ -103,19 +76,8 @@ export const DatePicker = ({ lang = "en", minDate, maxDate }) => {
     }
 
 
-    // const isSameDay = (date1, date2) => {
-    //     return (
-    //         date1.getDate() === date2.getDate() &&
-    //         date1.getMonth() === date2.getMonth() &&
-    //         date1.getFullYear() === date2.getFullYear()
-    //     )
-    // }
-
-    // const isDateInRange = (date) => {
-    //     // Check if the date is within the min and max range
-    //     return ((minDate && date > minDate) || (maxDate && date < maxDate))
-    // }
-
+    const months = t("months", { returnObjects: true })
+    const weekdays = t("weekdays", { returnObjects: true })
     return (
         <div className="relative flex justify-center mt-30 min-h-screen">
             <div>
@@ -133,100 +95,35 @@ export const DatePicker = ({ lang = "en", minDate, maxDate }) => {
 
                 {isOpen && (
                     <div className="absolute mt-2 w-64 bg-white border shadow-lg py-4 px-2 rounded-lg z-20">
-
-                        <div className="flex justify-between items-center mb-2">
-
-                            <button onClick={goToPreviousMonth} className="text-gray-500 hover:text-black cursor-pointer">&lt;</button>
-                            <div className="flex items-center gap-2">
-                                <select
-                                    value={currentDate.getMonth()}
-                                    onChange={(e) => {
-                                        const newDate = new Date(currentDate)
-                                        newDate.setMonth(Number(e.target.value))
-                                        setCurrentDate(newDate)
-                                    }}
-                                    className="border rounded px-2 py-1  text-sm"
-                                >
-                                    {t("months", { returnObjects: true }).map((month, index) => (
-                                        <option key={index} value={index}>
-                                            {month}
-                                        </option>
-                                    ))}
-                                </select>
-
-                                <select
-                                    value={currentDate.getFullYear()}
-                                    onChange={(e) => {
-                                        const newDate = new Date(currentDate)
-                                        newDate.setFullYear(Number(e.target.value))
-                                        setCurrentDate(newDate)
-                                    }}
-                                    className="border rounded px-2 py-1 text-sm"
-                                >
-                                    {Array.from({ length: 200 }, (_, i) => {
-                                        const year = new Date().getFullYear() - 100 + i
-                                        return (
-                                            <option key={year} value={year}>
-                                                {year}
-                                            </option>
-                                        )
-                                    })}
-                                </select>
-                            </div>
-
-                            <div className="text-right  items-center ">
-                                <button
-                                    onClick={() => setCurrentDate(new Date())}
-                                    className="text-blue-600 text-sm  cursor-pointer"
-                                    title={t("today")}
-                                >
-                                    <FontAwesomeIcon icon={faHome} />
-                                </button>
-                            </div>
-                            <button onClick={goToNextMonth} className="text-gray-500 hover:text-black cursor-pointer">&gt;</button>
-                        </div>
-
-
-
-                        <div className="grid grid-cols-7 gap-1 text-center text-sm font-semibold text-gray-600">
-                            {t("weekdays", { returnObjects: true }).map((day, idx) => (
-                                <div key={idx}>{day}</div>
-                            ))}
-                        </div>
-
-
-                        <div className="grid grid-cols-7 gap-1 text-center mt-2">
-                            {daysArray.map((day, index) => {
-                                const dateToCompare = new Date(currentDate.getFullYear(), currentDate.getMonth(), day)
-                                const today = new Date()
-                                const selected = selectedDate ? new Date(currentDate) : null
-                                const isDisabled = (!isDateInRange(dateToCompare) || !day)
-
-                                const isSelected =
-                                    selectedDate &&
-                                    isSameDay(dateToCompare, selected)
-
-                                const isToday =
-                                    !selectedDate &&
-                                    isSameDay(dateToCompare, today)
-
-                                const dayClasses = `p-2 w-8 h-8 flex items-center justify-center rounded-full
-                            ${day ? "cursor-pointer" : ""}
-                            ${isSelected || isToday ? "bg-blue-500 text-white" : ""}
-                            ${isDisabled ? "text-gray-400 cursor-not-allowed opacity-50" : "hover:bg-blue-400 hover:text-white"}
-                            `
-
-                                return (
-                                    <div
-                                        key={index}
-                                        className={dayClasses}
-                                        onClick={() => !isDisabled && day && handleDateClick(day)}
-                                    >
-                                        {day}
-                                    </div>
-                                )
-                            })}
-                        </div>
+                        <CalendarHeader
+                            currentMonth={currentDate.getMonth()}
+                            currentYear={currentDate.getFullYear()}
+                            months={months}
+                            goToPreviousMonth={goToPreviousMonth}
+                            goToNextMonth={goToNextMonth}
+                            onMonthChange={e => {
+                                const newDate = new Date(currentDate)
+                                newDate.setMonth(Number(e.target.value))
+                                setCurrentDate(newDate)
+                            }}
+                            onYearChange={e => {
+                                const newDate = new Date(currentDate)
+                                newDate.setFullYear(Number(e.target.value))
+                                setCurrentDate(newDate)
+                            }}
+                            setCurrentDate={setCurrentDate}
+                            t={t}
+                        />
+                        <Weekdays weekdays={weekdays} />
+                        <CalendarGrid
+                            daysArray={daysArray}
+                            handleDateClick={handleDateClick}
+                            currentDate={currentDate}
+                            selectedDate={selectedDate}
+                            minDate={minDate}
+                            maxDate={maxDate}
+                            lang={lang}
+                        />
                     </div>
                 )}
             </div>
